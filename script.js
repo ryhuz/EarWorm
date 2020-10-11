cabbe4e5aad34df12d7c1a8418adf4629eefa4dc
ryhuz <shawn.wee90@hotmail.com>
2020-10-11 17:07:43 +0800

    fills up artist and genre on 2nd choice page

--- script.js
+++ script.js
@@ -72,6 +72,154 @@ let library = [
                         ]
                 }
         ]
+    },
+    {   artist: "Taylor Swift",
+        genre: "Pop",
+        artistThumb: "/resources/artistthumb/ts.jpeg",
+        albums:[{   title: "Fearless",
+                    tracks:[
+                            {   tracktitle: "Love Story",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "You Belong With Me",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Tell Me Why",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                },
+                {   title: "Speak Now",
+                    tracks:[
+                            {   tracktitle: "Mine",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Sparks Fly",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Back To December",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                }
+        ]
+    },
+    {   artist: "Westlife",
+        genre: "Pop",
+        artistThumb: "/resources/artistthumb/westlife.jpeg",
+        albums:[{   title: "Westlife",
+                    tracks:[
+                            {   tracktitle: "Fool Again",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Flying Without Wings",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "More Than Words",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                },
+                {   title: "Coast To Coast",
+                    tracks:[
+                            {   tracktitle: "My Love",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "What Makes A Man",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "I Lay My Love On You",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                }
+        ]
+    },
+    {   artist: "Avenged Sevenfold",
+        genre: "Metal",
+        artistThumb: "/resources/artistthumb/a7x.jpeg",
+        albums:[{   title: "Waking The Fallen",
+                    tracks:[
+                            {   tracktitle: "Unholy Confessions",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Chapter Four",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Eternal Rest",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                },
+                {   title: "City Of Evil",
+                    tracks:[
+                            {   tracktitle: "Beast And The Harlot",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Bat Country",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Seize The Day",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                }
+        ]
+    },
+    {   artist: "blessthefall",
+        genre: "Metal",
+        artistThumb: "/resources/artistthumb/btf.jpeg",
+        albums:[{   title: "His Last Walk",
+                    tracks:[
+                            {   tracktitle: "Guys Like You Make Us Look Bad",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Higinia",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Rise Up",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                },
+                {   title: "Witness",
+                    tracks:[
+                            {   tracktitle: "What's Left Of Me",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "To Hell & Back",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "God Wears Gucci",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                }
+        ]
     }
 ];
 let mode = "";
@@ -79,7 +227,44 @@ let diff = "";
 let genre = "";
 let artist = "";
 
+// get artists/genres from library and populate the choice menu
+function fillArtist(){
+    let cArtist = $("#artist-select");
+
+    library.forEach ((x, index) =>{
+        let thumbPath = x.artistThumb;
+        let image = document.createElement("img");
+        image.setAttribute("src", thumbPath);
+        image.setAttribute("data-id", index);
+        cArtist.append(image);
+    });
+};
 fillArtist();
+
+function fillGenre(){
+    let cGenre = $("#genre-select");
+
+    library.forEach (x =>{
+        let curr = x.genre
+        let chk = document.getElementsByClassName("genre-option");
+        let exists = false;
+
+        for (let y of chk){
+            if (y.getAttribute("data-id") == curr){
+                exists = true;
+            }
+        }
+        
+        if (!exists){
+            let genre = document.createElement("div");
+            genre.setAttribute("class", "genre-option");
+            genre.setAttribute("data-id", curr);
+            genre.innerText = curr;
+            cGenre.append(genre);
+        }
+    });
+};
+fillGenre();
 
 // selecting options
 $(".mode-option").click(function(){
@@ -154,20 +339,19 @@ $("#start").click(function(){
 
         setTimeout(function () {
             if (mode == "artist"){
-                
                 cArtist.show();
             }else if (mode == "genre"){
-
+                cGenre.show();
             }else {
                 cArtist.show();
-                
+                cGenre.show();
             }
         }, 500);
     }
 });
 
 // Back to menu button
-$("#back").click(function(){
+$(".back").click(function(){
     let box = $(this).parent().parent().parent();
     
         box.hide();
@@ -177,19 +361,8 @@ $("#back").click(function(){
             $("#options").show();          
         }, 500);
 });
-// get artists/genres from library and populate the choice menu
 
-function fillArtist(){
-    let cArtist = $("#artist-select");
 
-    library.forEach ((x, index) =>{
-        let thumbPath = x.artistThumb;
-        let image = document.createElement("img");
-        image.setAttribute("src", thumbPath);
-        image.setAttribute("data-id", index);
-        cArtist.append(image);
-    });
-};
 
 /* /* function tranIn(x) {
     x.show();
@@ -208,7 +381,3 @@ function tranOut(x) {
 } */
 
 // set options as buttons, put data-id of the artist/genre in the respective boxes
-
-
-
-console.log(library);
