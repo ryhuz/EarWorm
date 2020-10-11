66f74693f3df1c080a736ed85a93dfaba33f07b8
ryhuz <shawn.wee90@hotmail.com>
2020-10-11 16:12:28 +0800

    artist options populated from library. transition effects not working

--- script.js
+++ script.js
@@ -1,9 +1,87 @@
+let library = [
+    {   artist: "Senses Fail",
+        artistThumb: "/resources/artistthumb/sensesfail.jpeg",
+        genre: "Rock",
+        albums:[{   title: "Let It Enfold You",
+                    tracks:[
+                            {   tracktitle: "Lady In A Blue Dress",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "You're Cute When Your Scream",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Bite To Break Skin",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                },
+                {   title: "Still Searching",
+                    tracks:[
+                            {   tracktitle: "Bonecrusher",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Can't Be Saved",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Calling All Cars",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                }
+        ]
+    },
+    {   artist: "Funeral For A Friend",
+        genre: "Rock",
+        artistThumb: "/resources/artistthumb/ffaf.jpeg",
+        albums:[{   title: "Casually Dressed And Deep In Conversation",
+                    tracks:[
+                            {   tracktitle: "Rookie Of The Year",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Juneau",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Bend Your Arms To Look Like Wings",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                },
+                {   title: "Hours",
+                    tracks:[
+                            {   tracktitle: "All The Rage",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Streetcar",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                            {   tracktitle: "Roses For The Dead",
+                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
+                                clip: ["clip 1", "clip 2", "clip 3"]
+                            },
+                        ]
+                }
+        ]
+    }
+];
 let mode = "";
 let diff = "";
 let genre = "";
 let artist = "";
 
-// on selecting options
+fillArtist();
+
+// selecting options
 $(".mode-option").click(function(){
     let box = $(this).parent().parent().parent().attr("id");
     let curr = $(this).attr("data-id");
@@ -46,7 +124,8 @@ $(".mode-option").click(function(){
     }
 });
 
-$(".start").click(function(){
+// clicking start
+$("#start").click(function(){
     // check if options are valid
     let str = "";
     if (mode == "" || diff == ""){
@@ -70,104 +149,66 @@ $(".start").click(function(){
         let cGenre = $("#choose-genre");
         
         // hide current windows and show 2nd option window
-        rules.on('animationend', () => {
-            rules.hide();
-            options.hide();
-            
+        options.hide();
+        rules.hide();
+
+        setTimeout(function () {
             if (mode == "artist"){
+                
                 cArtist.show();
-                cArtist.addClass("animate__animated animate__backInUp animate__slow");
             }else if (mode == "genre"){
-                cGenre.show();
-                cGenre.addClass("animate__animated animate__backInUp animate__slow");
+
             }else {
                 cArtist.show();
-                cArtist.addClass("animate__animated animate__backInUp animate__slow");
-                cGenre.show();
-                cGenre.addClass("animate__animated animate__backInUp animate__slow");
+                
             }
-        });
-        options.addClass("animate__animated animate__backOutDown animate__slow");
-        rules.addClass("animate__animated animate__backOutDown animate__slow");
+        }, 500);
     }
 });
 
+// Back to menu button
+$("#back").click(function(){
+    let box = $(this).parent().parent().parent();
+    
+        box.hide();
+
+        setTimeout( function() {
+            $("#rules").show();
+            $("#options").show();          
+        }, 500);
+});
 // get artists/genres from library and populate the choice menu
+
+function fillArtist(){
+    let cArtist = $("#artist-select");
+
+    library.forEach ((x, index) =>{
+        let thumbPath = x.artistThumb;
+        let image = document.createElement("img");
+        image.setAttribute("src", thumbPath);
+        image.setAttribute("data-id", index);
+        cArtist.append(image);
+    });
+};
+
+/* /* function tranIn(x) {
+    x.show();
+    x.addClass("animate__animated animate__backInUp animate__slow");
+    x.on('animationend', () => {
+        x.removeClass("animate__animated animate__backInUp animate__slow");
+    });
+}
+
+function tranOut(x) {
+    x.on('animationend', () => {
+        x.hide();
+        x.removeClass("animate__animated animate__backOutDown animate__slow");
+    });
+    x.addClass("animate__animated animate__backOutDown animate__slow");
+} */
+
 // set options as buttons, put data-id of the artist/genre in the respective boxes
 
-let library = [
-    {   artist: "Senses Fail",
-        genre: "Rock",
-        albums:[{   title: "Let It Enfold You",
-                    tracks:[
-                            {   tracktitle: "Lady In A Blue Dress",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "You're Cute When Your Scream",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "Bite To Break Skin",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                        ]
-                },
-                {   title: "Still Searching",
-                    tracks:[
-                            {   tracktitle: "Bonecrusher",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "Can't Be Saved",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "Calling All Cars",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                        ]
-                }
-        ]
-    },
-    {   artist: "Funeral For A Friend",
-        genre: "Rock",
-        albums:[{   title: "Casually Dressed And Deep In Conversation",
-                    tracks:[
-                            {   tracktitle: "Rookie Of The Year",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "Juneau",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "Bend Your Arms To Look Like Wings",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                        ]
-                },
-                {   title: "Hours",
-                    tracks:[
-                            {   tracktitle: "All The Rage",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "Streetcar",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                            {   tracktitle: "Roses For The Dead",
-                                lyrics: ["excerpt 1", "excerpt 1", "excerpt 1"],
-                                clip: ["clip 1", "clip 2", "clip 3"]
-                            },
-                        ]
-                }
-        ]
-    }
-];
+
 
 console.log(library);
